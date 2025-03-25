35422610b14a8006a2d9576f51b8b293c2d2f7a6
yuwenzh1 <153892421+xbaipang@users.noreply.github.com>
2025-03-25 17:42:23 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,42 @@
-tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        primaryHigh: "#000000",
-                        primaryLow: "#3B3B3B",
-                        secondary: "#858585",
-                        disabled: "#9C9C9C",
-                        primary: {
-                            normal: "#992E8A",
-                            hover: "#7A126B",
-                            pressed: "#4D144A",
-                        },
-                        secondaryColor: {
-                            normal: "#F2F3F5",
-                            hover: "#E5E7EC",
-                            pressed: "#C9CDD4",
-                        },
-                        other: {
-                            disabled: "#ECECEC",
-                            float: "#FFFFFF",
-                            card: "#F7F8FA",
-                            line: "#E5E7EC",
-                            background: "#FFFFFF",
-                        },
-                    },
-                    borderRadius: {
-                        button: "2px",
-                        card: "4px",
-                    },
-                    fontFamily: {
-                        sans: ["Segoe UI Variable", "Microsoft YaHei", "sans-serif"],
-                    },
-                    fontWeight: {
-                        normal: "400",
-                        semibold: "600",
-                        bold: "700",
-                    },
-                },
-            },
-        };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,css}", // 处理 src 目录下的 HTML、JS、TSX 文件
+    "./index.html" // 处理根目录中的 HTML 文件
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "body-en-regular-font-family": "Arial-Regular, sans-serif",
+        en: "Arial-Regular, sans-serif",
+      },
+      fontSize: {
+        "body-en-regular-font-size": "14px",
+        14: "14px",
+      },
+      fontWeight: {
+        "body-en-regular-font-weight": "400",
+      },
+      lineHeight: {
+        "body-en-regular-line-height": "22px",
+        22: "22px",
+      },
+      letterSpacing: {},
+      borderRadius: {
+        "radius-button-radius-small": "2px",
+      },
+      colors: {
+        "component-button-primary-normal": "#992e8a",
+        "semantic-text-other-white-back": "#ffffff",
+      },
+      spacing: {},
+      width: {},
+      minWidth: {},
+      maxWidth: {},
+      height: {},
+      minHeight: {},
+      maxHeight: {},
+    },
+  },
+  plugins: [],
+};
