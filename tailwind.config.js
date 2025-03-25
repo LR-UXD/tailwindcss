2e5d927fd5149410ea89dc93ae76f1d8f5e9068a
yuwenzh1 <153892421+xbaipang@users.noreply.github.com>
2025-03-25 13:52:52 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"], // 监测你的 HTML 和 JS 文件
-    theme: {
-      extend: {
-        colors: {
-          primary: "#992E8A", // 自定义颜色
-        },
-        fontFamily: {
-          sans: ["Inter", "sans-serif"], // 自定义字体
-        },
+window.tailwindConfig = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#992E8A",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
