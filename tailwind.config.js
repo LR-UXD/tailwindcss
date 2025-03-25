8ecfa0624abf8119f5f0bfa76b3467f6f013b774
yuwenzh1 <153892421+xbaipang@users.noreply.github.com>
2025-03-25 15:13:03 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,41 @@
-window.tailwindConfig = {
-    theme: {
-        extend: {
-            colors: {
-                primaryHigh: "#000000",
-                primaryLow: "#3B3B3B",
-                secondary: "#858585",
-                disabled: "#9C9C9C",
-                primary: {
-                    normal: "#992E8A",
-                    hover: "#7A126B",
-                    pressed: "#4D144A",
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primaryHigh: "#000000",
+                        primaryLow: "#3B3B3B",
+                        secondary: "#858585",
+                        disabled: "#9C9C9C",
+                        primary: {
+                            normal: "#992E8A",
+                            hover: "#7A126B",
+                            pressed: "#4D144A",
+                        },
+                        secondaryColor: {
+                            normal: "#F2F3F5",
+                            hover: "#E5E7EC",
+                            pressed: "#C9CDD4",
+                        },
+                        other: {
+                            disabled: "#ECECEC",
+                            float: "#FFFFFF",
+                            card: "#F7F8FA",
+                            line: "#E5E7EC",
+                            background: "#FFFFFF",
+                        },
+                    },
+                    borderRadius: {
+                        button: "2px",
+                        card: "4px",
+                    },
+                    fontFamily: {
+                        sans: ["Segoe UI Variable", "Microsoft YaHei", "sans-serif"],
+                    },
+                    fontWeight: {
+                        normal: "400",
+                        semibold: "600",
+                        bold: "700",
+                    },
                 },
-                secondaryColor: {
-                    normal: "#F2F3F5",
-                    hover: "#E5E7EC",
-                    pressed: "#C9CDD4",
-                },
-                other: {
-                    disabled: "#ECECEC",
-                    float: "#FFFFFF",
-                    card: "#F7F8FA",
-                    line: "#E5E7EC",
-                    background: "#FFFFFF",
-                },
             },
-            borderRadius: {
-                button: "2px",
-                card: "4px",
-            },
-            fontFamily: {
-                sans: ["Segoe UI Variable", "Microsoft YaHei", "sans-serif"],
-            },
-            fontWeight: {
-                normal: "400",
-                semibold: "600",
-                bold: "700",
-            },
-        },
-    },
-};
+        };
